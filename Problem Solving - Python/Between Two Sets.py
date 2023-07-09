e45a853d929ac7c43763a4773e050854e0a32237
Aazim Ahmed <108620737+Aazim10@users.noreply.github.com>
2023-07-09 18:20:15 +0500

    Create Between Two Sets.py

---
+++ Problem Solving - Python/Between Two Sets.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def getTotalX(a, b):
+    x=max(a)
+    y=min(b)
+    r= False
+    c=0
+    for i in range(x,y+1):
+        for j in range(len(a)):
+            if i%a[j]==0:
+                r = True
+            else:
+                r = False
+                break
+        if r:
+            for k in range(len(b)):
+                if b[k]%i==0:
+                    r = True
+                else:
+                    r = False
+                    break
+        if r:
+            c= c+1
+    return c
