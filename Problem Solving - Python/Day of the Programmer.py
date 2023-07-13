61e749598ea2f58b44cbcf99b50daa62eaafe6fc
Aazim Ahmed <108620737+Aazim10@users.noreply.github.com>
2023-07-13 17:13:56 +0500

    Create Day of the Programmer.py

---
+++ Problem Solving - Python/Day of the Programmer.py
@@ -0,0 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+def dayOfProgrammer(year):
+    if year < 1700 or year > 2700:
+        return       
+    elif year==1918:
+        return '26.09.1918'
+    elif year < 1918:
+        if year%4==0:
+            return '12.09.'+str(year)         
+        else:
+            return '13.09.'+str(year)          
+    elif year > 1918:
+        if year%400==0 or (year%4==0 and year%100!=0):
+            return '12.09.'+str(year) 
+        else:
+            return '13.09.'+str(year)
